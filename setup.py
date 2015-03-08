7f4e9c6eb952ed3ed0a03b88af81c96ba8a88153
neo <andsonliang@gmail.com>
2015-03-08 15:25:01 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+setup(name='jsontolua',
+	version='1.0',
+	description='a lib that conver json to lua table',
+	author='neoliang',
+	url='https://github.com/neoliang/jsontolua',
+	license='LGPL',
+	py_modules=['jsontolua'],
+	)
